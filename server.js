ccc4805cab60f9e759019aaf5417caff3a90e98c
Eliot Glairon <ejg_dnd@yahoo.com>
2017-07-02 15:26:21 -0600

    Moved settings to environment settings

--- server.js
+++ server.js
@@ -4,8 +4,11 @@
 const SERVER = "test-eliotn.c9users.io";
 const PORT = process.env.PORT || 3000;
 const DROP_DATA = true;
-const MONGO_URL = 'mongodb://localhost:27017/twitchvotes';
-const TWITCH_CLIENT_ID = "cmavc0iwqxq8zesuuvwca02gzaxoth";
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/twitchvotes';
+const TWITCH_CLIENT_ID = process.env.TWITCH_CLIENT_ID;
+const OAUTH_SECRET = process.env.OAUTH_SECRET;
+const TWITCH_SECRET = process.env.TWITCH_SECRET;
+
 
 //express
 const express = require('express');
@@ -109,12 +112,12 @@ var request = require('request');
 var client;
 const fs = require('fs');
 var channelToID = {};
-fs.readFile("secret-oauth", "utf-8", function(err, oauth-pwd) {
+fs.readFile("secret-oauth", "utf-8", function(err, oauthpwd) {
     if (err) throw err;
     client = new tmi.client({
         identity: {
             username: "ejg_dnd",
-            password: oauth-pwd
+            password: oauthpwd
         },
         channels: []
     });
@@ -165,130 +168,119 @@ var twitchStrategy = require("passport-twitch").Strategy;
 var BearerStrategy = require("passport-http-bearer").Strategy;
 
 //read secret from file before starting passport
-fs.readFile("secret", "utf-8", function(err, clientSecret) {
-    if (err) throw err;
-    passport.use(new twitchStrategy({
-        clientID: TWITCH_CLIENT_ID,
-        clientSecret: clientSecret,
-        callbackURL: "https://" + SERVER + "/auth/twitch/callback",
-        scope: "user_read",
-        session: false
-    }, function(accessToken, refreshToken, profile, done) {
-        //do stuff with the user after you log them in
-        users.findOne({"userid": profile.id}, function(err, results) {
-            if (err) {
-                return done(err);
-            }
-            if (!results) {
-                console.log("user needs to be created");
-                users.insert({"userid": profile.id, "access_token":accessToken, "tokenUpdateTime":new Date()}, function (err) {
-                    if (err) return done(err);
-                    return done(null, {"access_token":accessToken});
-                });
-            }
-            else {
-                console.log("user already logged in!");
-                users.updateOne({"userid": profile.id}, {$set: {"access_token":accessToken, "tokenUpdateTime":new Date()}});
-                
+passport.use(new twitchStrategy({
+    clientID: TWITCH_CLIENT_ID,
+    clientSecret: TWITCH_SECRET,
+    callbackURL: "https://" + SERVER + "/auth/twitch/callback",
+    scope: "user_read",
+    session: false
+}, function(accessToken, refreshToken, profile, done) {
+    //do stuff with the user after you log them in
+    users.findOne({"userid": profile.id}, function(err, results) {
+        if (err) {
+            return done(err);
+        }
+        if (!results) {
+            console.log("user needs to be created");
+            users.insert({"userid": profile.id, "access_token":accessToken, "tokenUpdateTime":new Date()}, function (err) {
+                if (err) return done(err);
                 return done(null, {"access_token":accessToken});
-            }
-        });
-        console.log(JSON.stringify(profile));
-        //put data we want access to in the callback here
-        
-    }));
-    afterPassport();
-});
-
-
-function afterPassport() {
-    
-    //authenticate based on token
-    passport.use(
-        new BearerStrategy(
-            function (token, done) {
-                if (!token) {
-                    console.log("token not found");
-                    return done(null, false, {message: 'no token'});
-                }
-                users.findOne({"access_token": token}, function(err, result) {
-                    if (err) {
-                        console.log("error in bearerstrategy" + err);
-                        return done(err);
-                    }
-                    if (!result) {
-                        console.log("incorrect token");
-                        return done(null, false, {message: 'incorrect token'});
-                    }
-                    console.log("correct token");
-                    return done(null, result, {scope:'all'});
-                });
-            }
-        )
-    );
-
-    //session is set to false because we aren't using sessions
-    //thanks https://jeroenpelgrims.com/token-based-sessionless-auth-using-express-and-passport
-    app.get('/auth/twitch/', passport.authenticate("twitch", {session: false}));
-    app.get('/auth/twitch/callback', passport.authenticate("twitch", {session:false, failureRedirect: '/'}), function (req, res) {
-    
-       res.redirect('/?access_token=' + req.user.access_token);
-    });
-    /*app.get('/', function (req, res) {
-        res.serveFile("Homepage");
-    });*/
-    app.put('/api/vote/:pollid/:vote', function (req, res) {
-        var voteresult = addVote(Number(req.params.pollid), req.params.vote-1);
-        if ("err" in voteresult) {
-            res.status(404).send();
-            return;
+            });
         }
-        res.json(voteresult)
-    });
-    app.get('/api/poll', passport.authenticate("bearer", {session: false}), function (req, res) {
-        polls.findOne({"userid": req.user.userid}, {}, function(err, results) {
-            if (err) {
-                res.json({"err":err});
-                return;
-            }
-            if (!results) {
-                res.json({});
-                return;
-            }
-            var object = {"question":results.question, "answers":results.answers, "votes":results.votes};
-            res.json(object);
-            return;
-        });
-    });
-    app.post('/api/poll', passport.authenticate("bearer", {session: false}), function (req, res) {
-        console.log(req.body);
-        if (req.body.question && req.body.answers) {
-            client.join("#ejg_dnd");
-            res.json(createPoll(Number(req.user.userid), req.body.question, req.body.answers));
-        }
-        
         else {
-            res.json({"Note":"You posted an invalid poll"});
+            console.log("user already logged in!");
+            users.updateOne({"userid": profile.id}, {$set: {"access_token":accessToken, "tokenUpdateTime":new Date()}});  
+            return done(null, {"access_token":accessToken});
         }
     });
-    app.get('/api/debug/testlogin',  passport.authenticate("bearer", {session: false}), function (req, res) {
-        res.json({"Note":"Your userid is " + req.user.userid});
-    });
-    //dump the database output
-    app.get('/api/debug/users',  passport.authenticate("bearer", {session: false}), function (req, res) {
-        users.find().toArray(function(err, result) {
-            if (err) throw err;
-            res.json({'users':JSON.stringify(result)}); 
-        });
-    });
-    app.get('/api/debug/polls',  passport.authenticate("bearer", {session: false}), function (req, res) {
-        polls.find().toArray(function(err, result) {
-            if (err) throw err;
-            res.json({'polls':JSON.stringify(result)}); 
-        });
-    });
+    console.log(JSON.stringify(profile));
+     //put data we want access to in the callback here
+        
+}));
+
+
+//authenticate based on token
+passport.use(
+    new BearerStrategy(
+        function (token, done) {
+            if (!token) {
+                console.log("token not found");
+                return done(null, false, {message: 'no token'});
+            }
+            users.findOne({"access_token": token}, function(err, result) {
+                if (err) {
+                    console.log("error in bearerstrategy" + err);
+                    return done(err);
+                }
+                if (!result) {
+                    console.log("incorrect token");
+                    return done(null, false, {message: 'incorrect token'});
+                }
+                console.log("correct token");
+                return done(null, result, {scope:'all'});
+            });
+        }
+    )
+);
+
+//session is set to false because we aren't using sessions
+//thanks https://jeroenpelgrims.com/token-based-sessionless-auth-using-express-and-passport
+app.get('/auth/twitch/', passport.authenticate("twitch", {session: false}));
+app.get('/auth/twitch/callback', passport.authenticate("twitch", {session:false, failureRedirect: '/'}), function (req, res) {
     
-}
+    res.redirect('/?access_token=' + req.user.access_token);
+});
+app.put('/api/vote/:pollid/:vote', function (req, res) {
+    var voteresult = addVote(Number(req.params.pollid), req.params.vote-1);
+    if ("err" in voteresult) {
+        res.status(404).send();
+        return;
+    }
+    res.json(voteresult)
+});
+app.get('/api/poll', passport.authenticate("bearer", {session: false}), function (req, res) {
+   polls.findOne({"userid": req.user.userid}, {}, function(err, results) {
+        if (err) {
+            res.json({"err":err});
+            return;
+        }
+        if (!results) {
+            res.json({});
+            return;
+        }
+        var object = {"question":results.question, "answers":results.answers, "votes":results.votes};
+        res.json(object);
+        return;
+    });
+});
+app.post('/api/poll', passport.authenticate("bearer", {session: false}), function (req, res) {
+    console.log(req.body);
+    if (req.body.question && req.body.answers) {
+        client.join("#ejg_dnd");
+        res.json(createPoll(Number(req.user.userid), req.body.question, req.body.answers));
+    }
+        
+    else {
+        res.json({"Note":"You posted an invalid poll"});
+    }
+});
+app.get('/api/debug/testlogin',  passport.authenticate("bearer", {session: false}), function (req, res) {
+    res.json({"Note":"Your userid is " + req.user.userid});
+});
+//dump the database output
+app.get('/api/debug/users',  passport.authenticate("bearer", {session: false}), function (req, res) {
+    users.find().toArray(function(err, result) {
+        if (err) throw err;
+        res.json({'users':JSON.stringify(result)}); 
+    });
+});
+app.get('/api/debug/polls',  passport.authenticate("bearer", {session: false}), function (req, res) {
+    polls.find().toArray(function(err, result) {
+        if (err) throw err;
+        res.json({'polls':JSON.stringify(result)}); 
+    });
+});
+    
 
 app.listen(PORT, function() {
     console.log("listening on port " + PORT);
